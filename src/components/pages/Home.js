97460747bbcab33e2bae134d4aa297a5ac397ec2
unknown <77635064+EMDevelop@users.noreply.github.com>
2021-04-27 23:23:31 +0100

    hero + why j2c test

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -49,7 +49,7 @@ function Home() {
 
   return (
     <>
-      <div className="MainBanner-container">
+      {/* <div className="MainBanner-container">
         <Grid container spacing={0} className={classes.grid}>
 
           
@@ -114,7 +114,7 @@ function Home() {
             </div>
           </Grid>
         </Grid>
-      </div>
+      </div> */}
 
       {/* ---------------------------------------------------------------------------------------------------------------------------- */}
 
