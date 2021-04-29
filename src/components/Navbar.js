90f414bb5c86a96bc7d817b054cd852fbc3466a0
unknown <77635064+EMDevelop@users.noreply.github.com>
2021-04-29 16:17:16 +0100

    New Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
 import {Navbar,Nav,NavDropdown,Button} from "react-bootstrap";
 import './Navbar.css'
-import MainLogo from '../images/Logo.png'
+// import MainLogo from '../images/Logo.png'
+import MainLogo from '../images/Just2Connect_Logo.png'
 import WhitePinkPhone from '../images/icons/WhitePinkPhone.png'
 
 function NavigationBar() {
